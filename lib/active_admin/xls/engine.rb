65dad7c708cae2130bb7bea61873fb34c2f1d701
Ubaldo Arias Flores <uariasflores@gmail.com>
2022-09-03 14:43:21 -0400

    Update engine.rb

--- lib/active_admin/xls/engine.rb
+++ lib/active_admin/xls/engine.rb
@@ -13,11 +13,15 @@ module ActiveAdmin
 
         ActiveAdmin::ResourceDSL.send :include, ActiveAdmin::Xls::DSL
         ActiveAdmin::Resource.send :include, ActiveAdmin::Xls::ResourceExtension
+      end
+      
+      ActiveSupport.on_load(:active_admin_controller) do
+        # Source | https://github.com/activeadmin/activeadmin/issues/7196#issuecomment-1132578565
         ActiveAdmin::ResourceController.send(
           :prepend,
           ActiveAdmin::Xls::ResourceControllerExtension
-        )
-      end
+        )        
+      end      
     end
   end
 end
